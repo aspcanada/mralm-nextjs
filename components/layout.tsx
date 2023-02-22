0b2d465afbc9d76634d7c323571a853b8dd184de
Alain St. Pierre <alain@aspcanada.com>
2023-02-21 20:19:36 -0800

    use dark sidebar

--- components/layout.tsx
+++ components/layout.tsx
@@ -12,7 +12,7 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
-import LogoLink from './logo'
+import LogoLink from './logoLink'
 
 const navigation = [
   { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
@@ -70,7 +70,7 @@ export default function Example() {
                 leaveFrom="translate-x-0"
                 leaveTo="-translate-x-full"
               >
-                <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-sky-700 pt-5 pb-4">
+                <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-gray-800 pt-5 pb-4">
                   <Transition.Child
                     as={Fragment}
                     enter="ease-in-out duration-300"
@@ -101,11 +101,19 @@ export default function Example() {
                           key={item.name}
                           href={item.href}
                           className={classNames(
-                            item.current ? 'bg-sky-800 text-white' : 'text-sky-100 hover:bg-sky-600',
+                            item.current
+                              ? 'bg-gray-900 text-white'
+                              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                             'group flex items-center px-2 py-2 text-base font-medium rounded-md'
                           )}
                         >
-                          <item.icon className="mr-4 h-6 w-6 flex-shrink-0 text-sky-300" aria-hidden="true" />
+                          <item.icon
+                            className={classNames(
+                              item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300',
+                              'mr-4 flex-shrink-0 h-6 w-6'
+                            )}
+                            aria-hidden="true"
+                          />
                           {item.name}
                         </a>
                       ))}
@@ -123,22 +131,28 @@ export default function Example() {
         {/* Static sidebar for desktop */}
         <div className="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex flex-grow flex-col overflow-y-auto bg-sky-700 pt-5">
-            <div className="flex flex-shrink-0 items-center px-4">
+          <div className="flex min-h-0 flex-1 flex-col bg-gray-800">
+            <div className="flex h-16 flex-shrink-0 items-center bg-gray-900 px-4">
               <LogoLink />
             </div>
-            <div className="mt-5 flex flex-1 flex-col">
-              <nav className="flex-1 space-y-1 px-2 pb-4">
+            <div className="flex flex-1 flex-col overflow-y-auto">
+              <nav className="flex-1 space-y-1 px-2 py-4">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
                     className={classNames(
-                      item.current ? 'bg-sky-800 text-white' : 'text-sky-100 hover:bg-sky-600',
+                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                       'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
                     )}
                   >
-                    <item.icon className="mr-3 h-6 w-6 flex-shrink-0 text-sky-300" aria-hidden="true" />
+                    <item.icon
+                      className={classNames(
+                        item.current ? 'text-sky-300' : 'text-sky-400 group-hover:text-sky-300',
+                        'mr-3 flex-shrink-0 h-6 w-6'
+                      )}
+                      aria-hidden="true"
+                    />
                     {item.name}
                   </a>
                 ))}
@@ -146,11 +160,11 @@ export default function Example() {
             </div>
           </div>
         </div>
-        <div className="flex flex-1 flex-col md:pl-64">
+        <div className="flex flex-col md:pl-64">
           <div className="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white shadow">
             <button
               type="button"
-              className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-sky-500 md:hidden"
+              className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
               onClick={() => setSidebarOpen(true)}
             >
               <span className="sr-only">Open sidebar</span>
@@ -179,7 +193,7 @@ export default function Example() {
               <div className="ml-4 flex items-center md:ml-6">
                 <button
                   type="button"
-                  className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
+                  className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
@@ -188,7 +202,7 @@ export default function Example() {
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2">
+                    <Menu.Button className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
@@ -229,7 +243,7 @@ export default function Example() {
             </div>
           </div>
 
-          <main>
+          <main className="flex-1">
             <div className="py-6">
               <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
                 <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
--- components/logoLink.tsx
+++ components/logoLink.tsx
@@ -7,11 +7,10 @@ export default function LogoLink() {
   return (
     <>
       <Link href="/">
-        <span className="sr-only">InvestDirect</span>
         <Image priority src={Logo} alt="" width={32} height={32} />
       </Link>
       <Link href="/">
-        <span className="text-2xl font-thin p-1">InvestDirect</span>
+        <span className="text-2xl font-thin p-1 text-white">InvestDirect</span>
       </Link>
     </>
   )
