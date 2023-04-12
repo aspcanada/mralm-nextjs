97fe8165720193e1b8da0d28f5fb7cdef8ea5f9e
Alain St. Pierre <alain@aspcanada.com>
2023-04-12 14:24:41 -0700

    fix nav on profile and settings page

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,163 +1,176 @@
-import { Fragment } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import {
   FolderIcon,
   HomeIcon,
   UsersIcon,
   XMarkIcon,
-} from '@heroicons/react/24/outline'
-import LogoLink from '../logoLink'
-import { classNames } from '../../utils/helpers'
-import Link from 'next/link'
-import { useRouter } from "next/router"
-import React, { useEffect, useState } from "react"
+} from "@heroicons/react/24/outline";
+import LogoLink from "../logoLink";
+import { classNames } from "../../utils/helpers";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 type SidebarProps = {
-  sidebarOpen: boolean
-  setSidebarOpen: (open: boolean) => void
-}
+  sidebarOpen: boolean;
+  setSidebarOpen: (open: boolean) => void;
+};
 
-export default function Sidebar({ sidebarOpen, setSidebarOpen}: SidebarProps) {
-  const router = useRouter()
-  const [activePage, setActivePage] = useState('/')
+export default function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarProps) {
+  const router = useRouter();
+  const [activePage, setActivePage] = useState("");
 
   const navigation = [
-    { name: 'Dashboard', href: '/', icon: HomeIcon, current: false },
-    { name: 'Deals', href: '/deals', icon: UsersIcon, current: false },
-    { name: 'Members', href: '/members', icon: FolderIcon, current: false },
+    { name: "Dashboard", href: "/dashboard", icon: HomeIcon, current: false },
+    { name: "Deals", href: "/deals", icon: UsersIcon, current: false },
+    { name: "Members", href: "/members", icon: FolderIcon, current: false },
     // { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
     // { name: 'Documents', href: '#', icon: InboxIcon, current: false },
     // { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
-  ]
+  ];
 
   useEffect(() => {
     if (router.asPath) {
       navigation.forEach((item) => {
         if (router.asPath.startsWith(item.href)) {
-          setActivePage(item.href)
+          setActivePage(item.href);
           // console.log('item.href', item.href)
         }
-      })
+      });
     }
-  }, [router])
+  }, [router]);
 
   return (
     <>
       {/* Static sidebar for mobile */}
       <Transition.Root show={sidebarOpen} as={Fragment}>
-          <Dialog as="div" className="relative z-40 md:hidden" onClose={setSidebarOpen}>
+        <Dialog
+          as="div"
+          className="relative z-40 md:hidden"
+          onClose={setSidebarOpen}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="transition-opacity ease-linear duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-40 flex">
             <Transition.Child
               as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
+              enter="transition ease-in-out duration-300 transform"
+              enterFrom="-translate-x-full"
+              enterTo="translate-x-0"
+              leave="transition ease-in-out duration-300 transform"
+              leaveFrom="translate-x-0"
+              leaveTo="-translate-x-full"
             >
-              <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-            </Transition.Child>
-
-            <div className="fixed inset-0 z-40 flex">
-              <Transition.Child
-                as={Fragment}
-                enter="transition ease-in-out duration-300 transform"
-                enterFrom="-translate-x-full"
-                enterTo="translate-x-0"
-                leave="transition ease-in-out duration-300 transform"
-                leaveFrom="translate-x-0"
-                leaveTo="-translate-x-full"
-              >
-                <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-gray-800 pt-5 pb-4">
-                  <Transition.Child
-                    as={Fragment}
-                    enter="ease-in-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in-out duration-300"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <div className="absolute top-0 right-0 -mr-12 pt-2">
-                      <button
-                        type="button"
-                        className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                        onClick={() => setSidebarOpen(false)}
-                      >
-                        <span className="sr-only">Close sidebar</span>
-                        <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                      </button>
-                    </div>
-                  </Transition.Child>
-                  <div className="flex flex-shrink-0 items-center px-4">
-                    <LogoLink />
+              <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-gray-800 pt-5 pb-4">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-in-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in-out duration-300"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="absolute top-0 right-0 -mr-12 pt-2">
+                    <button
+                      type="button"
+                      className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                      onClick={() => setSidebarOpen(false)}
+                    >
+                      <span className="sr-only">Close sidebar</span>
+                      <XMarkIcon
+                        className="h-6 w-6 text-white"
+                        aria-hidden="true"
+                      />
+                    </button>
                   </div>
-                  <div className="mt-5 h-0 flex-1 overflow-y-auto">
-                    <nav className="space-y-1 px-2">
-                      {navigation.map((item) => (
-                        <Link
-                          key={item.name}
-                          href={item.href}
+                </Transition.Child>
+                <div className="flex flex-shrink-0 items-center px-4">
+                  <LogoLink />
+                </div>
+                <div className="mt-5 h-0 flex-1 overflow-y-auto">
+                  <nav className="space-y-1 px-2">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                        )}
+                      >
+                        <item.icon
                           className={classNames(
                             item.current
-                              ? 'bg-gray-900 text-white'
-                              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                            'group flex items-center px-2 py-2 text-base font-medium rounded-md'
+                              ? "text-gray-300"
+                              : "text-gray-400 group-hover:text-gray-300",
+                            "mr-4 flex-shrink-0 h-6 w-6"
                           )}
-                        >
-                          <item.icon
-                            className={classNames(
-                              item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300',
-                              'mr-4 flex-shrink-0 h-6 w-6'
-                            )}
-                            aria-hidden="true"
-                          />
-                          {item.name}
-                        </Link>
-                      ))}
-                    </nav>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-              <div className="w-14 flex-shrink-0" aria-hidden="true">
-                {/* Dummy element to force sidebar to shrink to fit close icon */}
-              </div>
-            </div>
-          </Dialog>
-        </Transition.Root>
-
-        {/* Static sidebar for desktop */}
-        <div className="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
-          {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex min-h-0 flex-1 flex-col bg-gray-800">
-            <div className="flex h-16 flex-shrink-0 items-center bg-gray-900 px-4">
-              <LogoLink />
-            </div>
-            <div className="flex flex-1 flex-col overflow-y-auto">
-              <nav className="flex-1 space-y-1 px-2 py-4">
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className={classNames(
-                      item.href === activePage ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
-                    )}
-                  >
-                    <item.icon
-                      className={classNames(
-                        item.current ? 'text-sky-300' : 'text-sky-400 group-hover:text-sky-300',
-                        'mr-3 flex-shrink-0 h-6 w-6'
-                      )}
-                      aria-hidden="true"
-                    />
-                    {item.name}
-                  </Link>
-                ))}
-              </nav>
+                          aria-hidden="true"
+                        />
+                        {item.name}
+                      </Link>
+                    ))}
+                  </nav>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+            <div className="w-14 flex-shrink-0" aria-hidden="true">
+              {/* Dummy element to force sidebar to shrink to fit close icon */}
             </div>
           </div>
+        </Dialog>
+      </Transition.Root>
+
+      {/* Static sidebar for desktop */}
+      <div className="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
+        {/* Sidebar component, swap this element with another sidebar if you like */}
+        <div className="flex min-h-0 flex-1 flex-col bg-gray-800">
+          <div className="flex h-16 flex-shrink-0 items-center bg-gray-900 px-4">
+            <LogoLink />
+          </div>
+          <div className="flex flex-1 flex-col overflow-y-auto">
+            <nav className="flex-1 space-y-1 px-2 py-4">
+              {navigation.map((item) => (
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.href === activePage
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                  )}
+                >
+                  <item.icon
+                    className={classNames(
+                      item.current
+                        ? "text-sky-300"
+                        : "text-sky-400 group-hover:text-sky-300",
+                      "mr-3 flex-shrink-0 h-6 w-6"
+                    )}
+                    aria-hidden="true"
+                  />
+                  {item.name}
+                </Link>
+              ))}
+            </nav>
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,17 @@
-// import { Inter } from '@next/font/google'
-import Layout from '@/components/layout'
+import { useRouter } from "next/router"
+import React, { useEffect } from "react"
 
-// const inter = Inter({ subsets: ['latin'] })
+export default function IndexPage() {
+  const router = useRouter()
 
-export default function DashboardPage() {
+  useEffect(() => {
+    router.replace("/dashboard")
+  }, [router])
+
   return (
     <>
-      <Layout>
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-          <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
-        </div>
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-          <div className="py-4">
-            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
-          </div>
-        </div>
-      </Layout>
+      {/* <Loader /> */}
     </>
   )
+  
 }
